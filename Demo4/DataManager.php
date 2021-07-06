7d1148adfed451e1b83134f69280fdd649ed0aec
MinTimmy <timmygood6590@gmail.com>
2021-07-06 21:09:36 +0800

    new

---
+++ Demo4/DataManager.php
@@ -0,0 +1,114 @@
+<?php
+class DatabaseFunction
+{
+    private $db_host = "localhost";
+    private $db_username = "root";
+    private $db_password = "zxc741963";
+    private $db_name = "phpboard";
+    public function __construct()
+    {
+        try{
+            $this->db_link = new PDO("mysql:host={$this->db_host};dbname={$this->db_name};charset=utf8", $this->db_username, $this->db_password);
+        }catch (PDOException $e){
+            print("Connection failed, error message: {$e->getMessage()}<br/>");
+            die();
+        }
+    }
+    public function get_username($ID)
+    {
+        $squery = "SELECT `username` FROM `user` WHERE `id` = '$ID'";
+        $r = $this->db_link->query($squery);
+        $r->execute();
+        $result = $r->fetchAll(PDO::FETCH_BOTH);
+        return $result[0][0];
+    }
+
+    public function get_board_message()
+    {
+        $db_result = $this->db_link->query("SELECT * FROM `board` ORDER BY `post_time` DESC LIMIT 0,100");
+
+        $db_result->execute();
+        // $board_result = $db_result->fetchAll(PDO::FETCH_BOTH);
+        
+        $post_array = array();
+
+        while($board_result = $db_result->fetch(PDO::FETCH_ASSOC)){
+            extract($board_result);
+            
+            $post_item = array(
+                "id" => $id,
+                "message" =>$message,
+                "post_time" =>$post_time,
+            "user_id" => $user_id
+            );
+            array_push($post_array, $post_item); 
+        }
+
+        print_r($post_array);
+        // $json_data = json_encode($post_array);
+        // echo $json_data;
+        return $board_result;
+    }
+
+    //  有 sql injection 問題
+    // Solution
+    // https://www.php.net/manual/en/pdo.prepared-statements.php
+    public function new_message($mes, $user_id)
+    {
+        $squery = "INSERT INTO `board`(`id`, `message`, `post_time`, `user_id`) VALUES (NULL,:mes,now(),:user_id)";
+        $stmt = $this->db_link->prepare($squery);
+        $stmt->bindParam(':mes',$mes);
+        $stmt->bindParam((':user_id'), $user_id);
+        $stmt->execute();
+
+    }
+    public function new_user($username, $password)
+    {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        echo $hash;
+        $squery = "INSERT INTO `user`(`id`, `username`, `password`) VALUE (NULL, :username, :password)";
+        $stmt = $this->db_link->prepare($squery);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $hash);
+        $stmt->execute();
+    }
+    public function check_username($username): bool
+    {
+        $squery = "SELECT * FROM `user` WHERE `username` = :username";
+        $stmt = $this->db_link->prepare($squery);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+        if(empty($re)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public  function login_check($username, $password): bool
+    {
+           $squery = "SELECT * FROM `user` WHERE `username` = :username";
+           $stmt = $this->db_link->prepare($squery);
+           $stmt->bindParam(':username', $username);
+           $stmt->execute();
+           $res = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+           if(empty($res)){
+               return false;
+           }
+           else{
+               if(password_verify($password, $res[0]['password'])){
+                   return true;
+               }
+               else{
+                   return false;
+               }
+           }
+    }
+
+}
+
+
+
